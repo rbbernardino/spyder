67f40e4355809af02e2853c187a462bb7e7c8dbd
Rodrigo Bernardino <rbbernardino@gmail.com>
2015-08-21 14:43:59 -0400

    adicionando arquivos do projeto unity

---
+++ Spider Spy/Assets/scripts/GenerateObject.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class GenerateObject : MonoBehaviour
+{
+	public GameObject bomb;
+	public GameObject item;
+	public float position = 0.35f;
+	public float var = 0.1f;
+	public float StartTime = 1;
+	public float RepeatTime = 1;
+	public float difUnit = 1f;
+	private Score _score;
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("Createbomb", StartTime, RepeatTime);
+		//_score = GameObject.FindGameObjectWithTag ("Score").GetComponent<Score> ();
+	}
+	
+	void Createbomb()
+	{
+		float pos = ((Random.value >= 0.5f) ? position : -position);
+		//		pos = Random.Range (pos - (var / 2), pos + (var / 2));
+		int r = Random.Range (1, 10);
+		switch (r) { // [min, max[
+		case (1): // 1 bomb
+			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+			break;
+		case (2): // 1 Item
+			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
+			break;
+		case (3): // 1 center bomb
+			Instantiate(bomb, transform.position, transform.rotation);
+			break;
+		case (4): // 1 center Item
+			Instantiate(item, transform.position, transform.rotation);
+			break;
+		case (5): // 1 bomb, 1 Item, 1 bomb
+			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(item, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 3 * difUnit ), transform.rotation);
+			Instantiate(bomb, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 6 * difUnit ), transform.rotation);
+			break;
+		case (6): // 1 Item, 1 bomb
+			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(bomb, transform.position - ( Vector3.right * pos ), transform.rotation);
+			break;
+		case (7): // 1 Item, 1 (Other side) bomb
+			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(bomb, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
+			break;
+		case (8): // 1 bomb, 1 item (Other side), 1 bomb (Other side)
+			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 3.5f * difUnit ), transform.rotation);
+			Instantiate(bomb, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 7 * difUnit ), transform.rotation);
+			break;
+		case (9): // 1 bomb lines, item (other side)
+			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(bomb, transform.position + ( Vector3.right * pos/2 ) + ( Vector3.up * 1 * difUnit ), transform.rotation);
+			Instantiate(bomb, transform.position + ( Vector3.up * 2 * difUnit ), transform.rotation);
+			Instantiate(bomb, transform.position - ( Vector3.right * pos/2 ) + ( Vector3.up * 3 * difUnit ), transform.rotation);
+			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 4 * difUnit ), transform.rotation);
+			break;
+		case (10): // many bombs center, item sides
+			Instantiate(bomb, transform.position, transform.rotation);
+			Instantiate(bomb, transform.position +  ( Vector3.up * 1 * difUnit ), transform.rotation );
+			Instantiate(bomb, transform.position +  ( Vector3.up * 2 * difUnit ), transform.rotation );
+			Instantiate(bomb, transform.position +  ( Vector3.up * 3 * difUnit ), transform.rotation );
+			Instantiate(bomb, transform.position +  ( Vector3.up * 4 * difUnit ), transform.rotation );
+			Instantiate(bomb, transform.position +  ( Vector3.up * 5 * difUnit ), transform.rotation );
+			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
+			Instantiate(item, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
+			break;
+		}
+		
+		//_score.UpScore ();
+	}
+}
---
+++ Spider Spy/Assets/scripts/SpecialEffectsHelper.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Creating instance of particles from code with no effort
+/// </summary>
+public class SpecialEffectsHelper : MonoBehaviour
+{
+	/// <summary>
+	/// Singleton
+	/// </summary>
+	public static SpecialEffectsHelper Instance;
+
+	public ParticleSystem ExplosionEffect;
+	
+	void Awake()
+	{
+		// Register the singleton
+		if (Instance != null) {
+			Debug.LogError ("Multiple instances of SpecialEffectsHelper!");
+		}
+		
+		Instance = this;
+	} 
+
+	
+	public void Explosion(Vector3 position)
+	{
+		// Fire in the sky
+		instantiate(ExplosionEffect, position);
+	}
+	
+	/// <summary>
+	/// Instantiate a Particle system from prefab
+	/// </summary>
+	/// <param name="prefab"></param>
+	/// <returns></returns>
+	private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position)
+	{
+		ParticleSystem newParticleSystem = Instantiate(
+			prefab,
+			position,
+			prefab.transform.rotation
+			//Quaternion.identity
+			) as ParticleSystem;
+		// Make sure it will be destroyed
+		Destroy(
+			newParticleSystem.gameObject,
+			newParticleSystem.startLifetime
+			);
+		
+		return newParticleSystem;
+	}
+}
