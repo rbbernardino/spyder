2a8f1b00700a4b69fc23335021d34bfe7f4f7c2a
Galleom <Mr.galleom@hotmail.com>
2015-08-26 21:05:22 -0400

    Revert "threatObject -> bomb"
    This reverts commit 5e73c1775efbf93ec0d62a6d1f7d1240673d7c38.

--- Spider Spy/Assets/scripts/GenerateObject.cs
+++ Spider Spy/Assets/scripts/GenerateObject.cs
@@ -9,64 +9,66 @@ public class GenerateObject : MonoBehaviour
 	public float RepeatTime = 1;
 	public float difUnit = 1f;
 
+	private GameObject threatObject;
 	private Score _score;
 	// Use this for initialization
 	void Start()
 	{
-		InvokeRepeating("CreateItems", StartTime, RepeatTime);
+		InvokeRepeating("CreatethreatObject", StartTime, RepeatTime);
 		//_score = GameObject.FindGameObjectWithTag ("Score").GetComponent<Score> ();
 	}
 	
-	void CreateItems(){
+	void CreatethreatObject()
+	{
 		float pos = ((Random.value >= 0.5f) ? position : -position);
-		GameObject bomb = gameObject.GetComponent<GetRandomObject>().GetRandomThreatObject ();
+		threatObject = gameObject.GetComponent<GetRandomObject>().GetRandomThreatObject ();
 		int r = Random.Range (1, 10);
 		switch (r) { // [min, max[
-		case (1): // 1 bomb
-			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+		case (1): // 1 threatObject
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos ), transform.rotation);
 			break;
 		case (2): // 1 Item
 			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
 			break;
-		case (3): // 1 center bomb
-			Instantiate(bomb, transform.position, transform.rotation);
+		case (3): // 1 center threatObject
+			Instantiate(threatObject, transform.position, transform.rotation);
 			break;
 		case (4): // 1 center Item
 			Instantiate(item, transform.position, transform.rotation);
 			break;
-		case (5): // 1 bomb, 1 Item, 1 bomb
-			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+		case (5): // 1 threatObject, 1 Item, 1 threatObject
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos ), transform.rotation);
 			Instantiate(item, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 3 * difUnit ), transform.rotation);
-			Instantiate(bomb, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 6 * difUnit ), transform.rotation);
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 6 * difUnit ), transform.rotation);
 			break;
-		case (6): // 1 Item, 1 bomb
+		case (6): // 1 Item, 1 threatObject
 			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
-			Instantiate(bomb, transform.position - ( Vector3.right * pos ), transform.rotation);
+			Instantiate(threatObject, transform.position - ( Vector3.right * pos ), transform.rotation);
 			break;
-		case (7): // 1 Item, 1 (Other side) bomb
+		case (7): // 1 Item, 1 (Other side) threatObject
 			Instantiate(item, transform.position + ( Vector3.right * pos ), transform.rotation);
-			Instantiate(bomb, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
+			Instantiate(threatObject, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
 			break;
-		case (8): // 1 bomb, 1 item (Other side), 1 bomb (Other side)
-			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
+		case (8): // 1 threatObject, 1 item (Other side), 1 threatObject (Other side)
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos ), transform.rotation);
 			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 3.5f * difUnit ), transform.rotation);
-			Instantiate(bomb, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 7 * difUnit ), transform.rotation);
+			Instantiate(threatObject, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 7 * difUnit ), transform.rotation);
 			break;
-		case (9): // 1 bomb lines, item (other side)
-			Instantiate(bomb, transform.position + ( Vector3.right * pos ), transform.rotation);
-			Instantiate(bomb, transform.position + ( Vector3.right * pos/2 ) + ( Vector3.up * 1 * difUnit ), transform.rotation);
-			Instantiate(bomb, transform.position + ( Vector3.up * 2 * difUnit ), transform.rotation);
-			Instantiate(bomb, transform.position - ( Vector3.right * pos/2 ) + ( Vector3.up * 3 * difUnit ), transform.rotation);
+		case (9): // 1 threatObject lines, item (other side)
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos ), transform.rotation);
+			Instantiate(threatObject, transform.position + ( Vector3.right * pos/2 ) + ( Vector3.up * 1 * difUnit ), transform.rotation);
+			Instantiate(threatObject, transform.position + ( Vector3.up * 2 * difUnit ), transform.rotation);
+			Instantiate(threatObject, transform.position - ( Vector3.right * pos/2 ) + ( Vector3.up * 3 * difUnit ), transform.rotation);
 			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 4 * difUnit ), transform.rotation);
 			break;
 /*
-		case (10): // many bombs center, item sides
-			Instantiate(bomb, transform.position, transform.rotation);
-			Instantiate(bomb, transform.position +  ( Vector3.up * 1 * difUnit ), transform.rotation );
-			Instantiate(bomb, transform.position +  ( Vector3.up * 2 * difUnit ), transform.rotation );
-			Instantiate(bomb, transform.position +  ( Vector3.up * 3 * difUnit ), transform.rotation );
-			Instantiate(bomb, transform.position +  ( Vector3.up * 4 * difUnit ), transform.rotation );
-			Instantiate(bomb, transform.position +  ( Vector3.up * 5 * difUnit ), transform.rotation );
+		case (10): // many threatObjects center, item sides
+			Instantiate(threatObject, transform.position, transform.rotation);
+			Instantiate(threatObject, transform.position +  ( Vector3.up * 1 * difUnit ), transform.rotation );
+			Instantiate(threatObject, transform.position +  ( Vector3.up * 2 * difUnit ), transform.rotation );
+			Instantiate(threatObject, transform.position +  ( Vector3.up * 3 * difUnit ), transform.rotation );
+			Instantiate(threatObject, transform.position +  ( Vector3.up * 4 * difUnit ), transform.rotation );
+			Instantiate(threatObject, transform.position +  ( Vector3.up * 5 * difUnit ), transform.rotation );
 			Instantiate(item, transform.position - ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
 			Instantiate(item, transform.position + ( Vector3.right * pos ) + ( Vector3.up * 5 * difUnit ), transform.rotation);
 			break;
