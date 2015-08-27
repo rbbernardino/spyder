acfc4f91d66d7664b047b81677d0be55d1e495a8
Rodrigo Bernardino <rbbernardino@gmail.com>
2015-08-27 15:05:46 -0400

    - Botão de pause - Script de settings movido para pasta Script - Renomeado algumas variáveis, scripts e GameObjects - Criado classe "GameplayControls" para encapsular funções de controle do jogo - Trocar abordagem "FindByTag" para settar variáveis no Unity .......GameObject Controller .......Script GameplayControls - Criado e organizado pastas para sons e desenhos

--- Spider Spy/Assets/scripts/Espiao/CheckCollision.cs
+++ Spider Spy/Assets/scripts/Espiao/CheckCollision.cs
@@ -2,24 +2,20 @@
 
 public class CheckCollision : MonoBehaviour
 {
-	private GameObject _menuPopUP;
-	private GameObject _player;
 	private bool isAlive;
 	private Score _score;
+    private GameplayControls gameplayControls;
 
 	public bool alive(){return isAlive;}
 	void Start(){	
 		isAlive = true;
-		_menuPopUP = GameObject.FindGameObjectWithTag ("MenuGameOver");
-		_player = GameObject.FindGameObjectWithTag ("Player");
 		_score = GameObject.FindGameObjectWithTag ("Score").GetComponent<Score> ();
-		_menuPopUP.SetActive (false);
+        gameplayControls = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameplayControls>();
+    }
 
-	}
-
-	// Die by collision
-	// Collider is on child objects
-	void OnTriggerEnter2D(Collider2D other){
+    // Die by collision
+    // Collider is on child objects
+    void OnTriggerEnter2D(Collider2D other){
 		string tag = other.gameObject.tag;
 		switch (tag) {
 		case ("Item"):
@@ -39,9 +35,9 @@ public class CheckCollision : MonoBehaviour
 	{
 		isAlive = false;
 		SpecialEffectsHelper.Instance.Explosion (transform.position);
-		//Application.LoadLevel(Application.loadedLevel);
-		_player.SetActive(false);
-		_score.SaveScore ();
-		_menuPopUP.SetActive (true);
+        //Application.LoadLevel(Application.loadedLevel);
+        _score.SaveScore ();
+        gameplayControls.ShowGameover();
+        gameplayControls.HidePlayer();
 	}
 }
---
+++ Spider Spy/Assets/scripts/GameplayControls.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayControls : MonoBehaviour {
+    [SerializeField] private GameObject menuPause;
+    [SerializeField] private GameObject menuGameOver;
+
+    private GameObject Player;
+
+    public void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        menuPause.SetActive(false);
+        menuGameOver.SetActive(false);
+    }
+
+    public void ShowGameover() { menuGameOver.SetActive(true); }
+    public void HideGameover() { menuGameOver.SetActive(false); }
+
+    public void ShowPauseMenu() { menuPause.SetActive(true); }
+    public void HidePauseMenu() { menuPause.SetActive(false); }
+
+    public void HidePlayer() { Player.SetActive(false); }
+
+    public void Pause()
+    {
+        //Set time.timescale to 0, this will cause animations and physics to stop updating
+        Time.timeScale = 0;
+        //Show pause menu
+        menuPause.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        menuPause.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
