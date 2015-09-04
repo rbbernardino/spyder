3dfce90ed7da76abb2be7f90e2f63bd0d13ddab7
Rodrigo Bernardino <rbbernardino@gmail.com>
2015-09-04 15:23:51 -0400

    Animação do menu melhorada (comprimida)

---
+++ Spider Spy/Assets/OnStop.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnStop : MonoBehaviour {
+	public string PauseSplashTAG;
+	private bool Paused;
+	private GameObject PauseSplash;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnApplicationPause(bool pauseState)
+	{
+		Paused = pauseState;
+		
+		if (Paused == true) {
+			Time.timeScale = 0;
+			AudioListener.volume = 0.0f;
+			//Caso desejem por Splash de pause no meio do jogo DESCOMENTAR abaixo
+//			PauseSplash=GameObject.FindGameObjectWithTag(PauseSplashTAG);
+//			PauseSplash.SetActive(true);
+		} else {
+			//Caso desejem por Splash de pause no meio do jogo COMENTAR abaixo
+			//(Seus "pauses gameobjects) devem despausar o jogo
+			Time.timeScale = 1;
+
+			AudioListener.volume = 1.0f;
+		}
+	}
+}
---
+++ Spider Spy/Assets/scripts/PlayVideo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+    public void Play()
+    {
+        Handheld.PlayFullScreenMovie("ss2.mp4", Color.black, FullScreenMovieControlMode.Full);
+        Application.LoadLevel("Main Menu");
+    }
+}
